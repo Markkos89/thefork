c4ee17873d0ca0ba31e00693d4ec4f3c9f22b70e
Marcos Iglesias <markkos89@gmail.com>
2019-11-15 16:22:27 -0300

    GET restaurants

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const fs = require('fs')
 const app = express()
 const apiPort = 3000
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -11,5 +13,14 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.send(`Hello TheFork! I'm coming!!`)
 })
+
+app.get('/restaurants', (req, res) => {
+    let rawdata = fs.readFileSync('data.json');
+    let restaurants = JSON.parse(rawdata);
+    console.log(restaurants);
+    res.send(restaurants)
+})
+
+
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
