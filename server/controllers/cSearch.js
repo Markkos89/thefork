f58bbbedf55f409c24535d72db7f68e97d60b490
Marcos Iglesias <markkos89@gmail.com>
2019-11-16 03:04:07 -0300

    Sorting Results

--- server/controllers/cSearch.js
+++ server/controllers/cSearch.js
@@ -6,6 +6,7 @@ exports.getApiSearch = async (req, res) => {
     const coor_x = parseInt(req.query.coor_x)
     const coor_y = parseInt(req.query.coor_y)
     const max_results = parseInt(req.query.max_results)
+    const sort_by = req.query.sort_by
 
     const restaurants = await helpers.getRestaurantsFromDataJson()
     const aDistances = []
@@ -15,15 +16,30 @@ exports.getApiSearch = async (req, res) => {
         restaurants[x].distance = dist
         aDistances.push(restaurants[x])
     }
-    // lo sorteo por la distancia con lodash
-    let aDistancesSorted = _.sortBy(aDistances, ['distance'], (o) => { return o })
 
+    // lo ordeno por la distancia con lodash
+    let aDistancesSorted = _.sortBy(aDistances, ['distance'], (o) => { return o })
     let aRestaurants = []
+
     // devuelvo solo la cantidad pedida por parametros
     for (let x = 0; x < max_results; x++) {
         let resto = aDistancesSorted[x]
         aRestaurants.push(resto)
     }
 
-    res.send({ count: aDistances.length, restaurants: aRestaurants })
+    let aRestaurantsSortedBy = []
+    // devuelvo el orden, si es que vino, por parametros
+    switch (sort_by) {
+        case 'name':
+            aRestaurantsSortedBy = _.orderBy(aRestaurants, ['name'])
+            break
+        case 'rating':
+            aRestaurantsSortedBy = _.orderBy(aRestaurants, ['rating'], ['desc'])
+            break
+        default:
+            res.send({ count: aDistances.length, restaurants: aRestaurants })
+            break
+    }
+
+    res.send({ count: aDistances.length, restaurants: aRestaurantsSortedBy })
 }
